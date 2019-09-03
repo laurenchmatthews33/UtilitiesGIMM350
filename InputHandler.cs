f3f26dc97e27e2f095821c1dc674f7b165aea082
laurenchmatthews33 <54817164+laurenchmatthews33@users.noreply.github.com>
2019-09-03 10:41:33 -0600

    Coding Challenge - 9/3/19 - ParseString

---
+++ InputHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class InputHandler : MonoBehaviour
+{
+    
+
+    public InputField inputText;
+    public Text outputText;
+    public Button textButton; 
+
+    private void Start()
+    {
+        outputText.text = ""; 
+    }
+
+    public void processText()
+    {
+        outputText.text = Utilities.ProcessText(inputText.text); 
+    }
+
+
+}
---
+++ Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static string ProcessText(string textIn)
+    {
+
+        double number;
+        double.TryParse(textIn, out number);
+
+        if (double.TryParse(textIn, out number)) {
+            return "Number"; }
+        else { return "String";
+            //return word string if the user enters a string
+            //return the work number if the user enters a number 
+
+
+            //if <> == string()
+            //Console.WriteLine("The textIn is a String)
+
+            //else
+            //if <> == number; 
+            //Colsole.WriteLine("The textIn is a Number")
+
+        }
+    }
+}
